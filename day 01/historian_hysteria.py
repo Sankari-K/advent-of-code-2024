a4d12ec9f2f275e590cb23635ebfd81a1d47116c
Sankari Karthik <sankarikarthik03#gmail.com>
2024-12-01 18:00:25 +0530

    Clean up soln

--- day 01/historian_hysteria.py
+++ day 01/historian_hysteria.py
@@ -7,17 +7,18 @@ def get_puzzle_input(directory):
     return [list(map(int, line.strip().split())) for line in lines]
 
 def find_list_distance(input):
-    left, right = sorted([i[0] for i in input]), sorted([i[1] for i in input])
+    left, right = map(sorted, zip(*input))
     return sum([abs(l - r) for l, r in zip(left, right)])
 
 def find_similarity_score(input):
-    left, right = [i[0] for i in input], Counter([i[1] for i in input])
+    left, right = zip(*input)
+    right = Counter(right)
     return sum([l * right[l] for l in left])
 
 # part a
 puzzle_input = get_puzzle_input(r"./puzzle_input.txt")
 print(find_list_distance(puzzle_input))
 
-# part a
+# part b
 puzzle_input = get_puzzle_input(r"./puzzle_input.txt")
 print(find_similarity_score(puzzle_input))
