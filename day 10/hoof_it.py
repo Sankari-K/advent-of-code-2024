bc80abf6bfbabc62a8029ada292ff47cba2a8a51
Sankari Karthik <sankarikarthik03#gmail.com>
2024-12-10 11:32:17 +0530

    Clean up soln for day 10

--- day 10/hoof_it.py
+++ day 10/hoof_it.py
@@ -1,50 +1,46 @@
+DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
 def get_puzzle_input(directory):
     with open(directory) as f:
         file = f.read().split()
     
-    topographic_map = dict()
+    TOPOGRAPHIC_MAP = dict()
     for x, height_row in enumerate(file):
         for y, height in enumerate(height_row):
-            topographic_map[(x, y)] = int(height)
-    return topographic_map, len(file), len(file[0])
+            TOPOGRAPHIC_MAP[(x, y)] = int(height)
+    return TOPOGRAPHIC_MAP, len(file), len(file[0])
 
-def get_hiking_trail_heads(topographic_map):
-    return [coordinate for coordinate, item in topographic_map.items() if item == 0]
+def get_hiking_trail_heads(TOPOGRAPHIC_MAP):
+    return [coordinate for coordinate, item in TOPOGRAPHIC_MAP.items() if item == 0]
 
 def get_score(x, y):
-    if topographic_map[(x, y)] == 9:
+    if TOPOGRAPHIC_MAP[(x, y)] == 9:
         return {(x, y)}
     
     ans = set()
-    for dx, dy in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+    for dx, dy in DIRECTIONS:
         new_x, new_y = x + dx, y + dy
-        if (new_x, new_y) in topographic_map and topographic_map[(new_x, new_y)] == topographic_map[(x, y)] + 1:
+        if (new_x, new_y) in TOPOGRAPHIC_MAP and TOPOGRAPHIC_MAP[(new_x, new_y)] == TOPOGRAPHIC_MAP[(x, y)] + 1:
             ans.update(get_score(new_x, new_y))
     return ans
 
 def get_rating(x, y):
-    if topographic_map[(x, y)] == 9:
+    if TOPOGRAPHIC_MAP[(x, y)] == 9:
         return 1
     
     ans = 0
-    for dx, dy in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+    for dx, dy in DIRECTIONS:
         new_x, new_y = x + dx, y + dy
-        if (new_x, new_y) in topographic_map and topographic_map[(new_x, new_y)] == topographic_map[(x, y)] + 1:
+        if (new_x, new_y) in TOPOGRAPHIC_MAP and TOPOGRAPHIC_MAP[(new_x, new_y)] == TOPOGRAPHIC_MAP[(x, y)] + 1:
             ans += get_rating(new_x, new_y)
     return ans
 
 def get_sum_scores():
-    sum_scores = 0
-    for trail_head in get_hiking_trail_heads(topographic_map):
-        sum_scores += len(get_score(*trail_head))
-    return sum_scores
+    return sum(map(lambda x: len(get_score(*x)), get_hiking_trail_heads(TOPOGRAPHIC_MAP)))
 
 def get_sum_ratings():
-    sum_scores = 0
-    for trail_head in get_hiking_trail_heads(topographic_map):
-        sum_scores += get_rating(*trail_head)
-    return sum_scores
+    return sum(map(lambda x: get_rating(*x), get_hiking_trail_heads(TOPOGRAPHIC_MAP)))
 
-topographic_map, X_LIMIT, Y_LIMIT = get_puzzle_input("./puzzle_input.txt")
+TOPOGRAPHIC_MAP, X_LIMIT, Y_LIMIT = get_puzzle_input("./puzzle_input.txt")
 print(get_sum_scores())
 print(get_sum_ratings())
