3f5d3c8b9a647642f77bce37018bb08507086141
Sankari Karthik <sankarikarthik03#gmail.com>
2024-12-13 09:27:26 +0530

    Add day 12 part 2 soln

--- day 12/garden_groups.py
+++ day 12/garden_groups.py
@@ -13,9 +13,9 @@ def get_puzzle_input(directory):
 
 def get_details(x, y, cycle):
     if (x, y) in cycle or (x, y) in SEEN:
-        return 0, 0
+        return 0, 0, []
     if (x, y) not in GARDEN:
-        return 0, 0
+        return 0, 0, []
     
     perimeter = 0
     area = 1
@@ -24,29 +24,64 @@ def get_details(x, y, cycle):
     if y == 0 or y == Y_LIMIT - 1:
         perimeter += 1
 
+    points = [(x, y)]
     cycle.add((x, y))
     for dx, dy in DIRECTIONS:
         newx, newy = x + dx, y + dy
         if (newx, newy) in GARDEN and GARDEN[(x, y)] != GARDEN[(newx, newy)]:
             perimeter += 1
         else:
-            extraarea, extraperimeter =  get_details(newx, newy, cycle)
+            extraarea, extraperimeter, extrapoints =  get_details(newx, newy, cycle)
             area += extraarea
             perimeter += extraperimeter
+            points += extrapoints
+
     SEEN.add((x, y))
     cycle.remove((x, y))
-    return area, perimeter
-    
-    
+    return area, perimeter, points
+     
 def get_fencing_price():
-    price = 0
+    price, discount_price = 0, 0
     for x in range(X_LIMIT):
         for y in range(Y_LIMIT):
             if (x, y) not in SEEN:
-                area, perimeter = get_details(x, y, set())
+                area, perimeter, points = get_details(x, y, set())
                 price += area * perimeter
+                discount_price += area * get_sides(points)
                 SEEN.add((x, y))
-    return price
+    return price, discount_price
+
+def get_sides(region):
+    up_sides = 0
+    down_sides = 0
+    left_sides = 0
+    right_sides = 0
+
+    for (x, y) in region:
+        left = x - 1
+        right = x + 1
+        above = y - 1
+        below = y + 1
+        right_not_in_region = (right, y) not in region
+        below_not_region = (x, below) not in region
+
+        if (x, above) not in region:
+            if right_not_in_region or (right, above) in region:
+                up_sides += 1
+
+        if (x, below) not in region:
+            if right_not_in_region or (right, below) in region:
+                down_sides += 1
+
+        if (left, y) not in region:
+            if below_not_region or (left, below) in region:
+                left_sides += 1
+
+        if (right, y) not in region:
+            if below_not_region or (right, below) in region:
+                right_sides += 1
+
+    return up_sides + down_sides + left_sides + right_sides
 
 X_LIMIT, Y_LIMIT, GARDEN = get_puzzle_input("./puzzle_input.txt")
 print(get_fencing_price())
